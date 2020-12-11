d0048c638b8b365f845f92efb4909224350a1129
Pradeep699 <75341192+Pradeep699@users.noreply.github.com>
2020-12-11 09:39:26 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Deployed Version is v1.0.Build in action, Hello world</h1>");
+  response.end("<h1> Deployed Version is v1.0.Build in action, Hello </h1>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
